242704b7a9f91037a2cc9a56d9e8eae007a6dae6
Ben Hanover <benhanover4@gmail.com>
2021-03-19 17:55:20 +0200

    hover photo

--- client/src/components/PokemonCard.js
+++ client/src/components/PokemonCard.js
@@ -4,13 +4,6 @@ import axios from 'axios';
 
 export default function PokemonCard({pokemon, getTypeList}) {
 
-    // const [typeList, setTypeList] = useState([]);
-
-    // async function  getTypeList (e) {
-    //     const type = e.target.innerText;
-    //     const res = await axios.get(`http://localhost:9000/api/type/${type}`);
-    //     setTypeList(res.data);
-    // }
 
     const { name, height, weight, id, back_default, front_default, types } = pokemon;
     const handledTypes = types.map(t => {
@@ -25,8 +18,12 @@ export default function PokemonCard({pokemon, getTypeList}) {
                 <span>height:</span><span>{` ${height}`}</span><br/>
                 <span>weight:</span><span>{` ${weight}`}</span><br/>
                 <span>types:</span> <span>{handledTypes}</span><br/>
-                <img src={back_default} />
-                <img src={front_default} />
+                <img
+                    src={front_default}
+                    onMouseOver = {e => e.currentTarget.src = back_default}
+                    onMouseOut = {e => e.currentTarget.src = front_default}
+                />
+            
             </div>
         
     )
--- client/src/components/Types.js
+++ client/src/components/Types.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Types({typeList, getPokemonByTypes}) {
 
 
-
   const newArr = typeList.map(type => <li onClick={getPokemonByTypes}>{type.pokemon.name}</li>);
     
 
