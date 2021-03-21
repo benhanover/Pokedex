8a5eb8827ee08a1697b47229ce8fdd24504d10f9
Ben Hanover <benhanover4@gmail.com>
2021-03-21 13:38:23 +0200

    comit before submit

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const api = require("./routes/routesControl");
-const path = require('path');
 
 app.use(express.json());
-app.use("/api", api);
-app.use(express.static('./client/public'))
+app.use(express.static('./client/build'));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.use("/api", api);
 
 
 module.exports = app;
